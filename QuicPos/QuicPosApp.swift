59d037f5cef46d743495b660288355ceea0556c7
Jakub Adamski <kuba@exemplum.pl>
2020-10-03 17:18:38 +0200

    Initial Commit

---
+++ QuicPos/QuicPosApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuicPosApp.swift
+//  QuicPos
+//
+//  Created by Kuba on 03/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct QuicPosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
